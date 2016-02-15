71b676ff635228680f78cff6cae87eb603a0c362
Nick Cummings <nickmcummings@gmail.com>
2016-02-15 12:38:38 -0800

    Created GM and basic GUI; added UI text update function

---
+++ Assets/GM.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GM : MonoBehaviour {
+
+	public int grass, wood;
+	public Text materials;
+
+	// Use this for initialization
+	void Start () {
+		grass = 0;
+		wood = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Will eventually move this out of Update() and call it only when values change 
+		updateGUI();
+	}
+
+	void updateGUI() {
+		materials.text = "Grass: " + grass + "\nWood: " + wood;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-The Carpenter
-My one-game-a-month game for February 2016
+#The Carpenter  
+My one-game-a-month game for February 2016  
+
+##Overview  
+The Carpenter is a tiny survival and crafting game. 

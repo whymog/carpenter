ed0c1b81cdfdc0374c41e6a9b5c0b7b6d4b0116a
Nick Cummings <nickmcummings@gmail.com>
2016-02-15 12:12:59 -0800

    Added basic horizontal sprite movement.

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	public float moveSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("left") && Input.GetKey ("right")) {
+			// do nothin'
+			return;
+		} else if (Input.GetKey ("left")) {
+			movePlayer (Vector3.left, moveSpeed);
+		} else if (Input.GetKey ("right")) {
+			movePlayer (Vector3.right, moveSpeed);
+		}
+	}
+
+	void movePlayer(Vector3 direction, float factor) {
+		transform.position += (direction * factor * Time.deltaTime);
+	}
+}
